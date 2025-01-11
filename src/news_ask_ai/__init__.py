d9f839229c32ed1884ae193d5b0453776e8404dd
doramasma <doramas6@hotmail.com>
2025-01-11 19:44:29 +0000

    Adding llm completion service

--- src/news_ask_ai/__init__.py
+++ src/news_ask_ai/__init__.py
@@ -1,12 +1,14 @@
 import json
 from news_ask_ai.services.chroma_service import ChromaDBService
 from news_ask_ai.services.embedding_service import EmbeddingService
+from news_ask_ai.services.llm_completion_service import LLMCompletionService
 
 
 def main() -> None:
     service = ChromaDBService("news-collection")
     embedding_service = EmbeddingService()
 
+    # Reading example news dataset
     with open("src/news_ask_ai/datasets/news_articles_dataset_example.json", "r", encoding="utf-8") as file:
         news_articles = json.load(file)
 
@@ -25,3 +27,16 @@ def main() -> None:
     query_embedding = embedding_service.get_embeddings([query])
     retrieve_documents = service.retrieve_documents(query_embedding)
     print(retrieve_documents)
+
+    # Example of using the LLM
+    service = LLMCompletionService()
+    messages = [
+        {"role": "system", "content": "You are a helpful AI assistant."},
+        {"role": "user", "content": "What about solving an 2x + 3 = 7 equation?"},
+    ]
+
+    try:
+        completion = service.get_completions(messages)
+        print(completion)
+    except Exception as e:
+        print(f"An error occurred: {e}")
